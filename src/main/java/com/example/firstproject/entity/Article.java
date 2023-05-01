3a0a8a136c03e01405511e86cf15ce8c89eadd9a
hbseog <lllhades@naver.com>
2023-05-01 21:36:51 +0900

    my first project

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,33 +1,24 @@
 package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 @Entity
+@Table(name = "ARTICLE")
 @SequenceGenerator(
         name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "member_seq",
+        sequenceName = "SEQ_NAME",
         allocationSize = 1
 )
 public class Article {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
-    @Column
+    @Column(name = "TITLE")
     private String title;
-    @Column
-    public String content;
-
-    public Article(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+    @Column(name = "CONTENT")
+    private String content;
 }
