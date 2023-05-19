350d97c10e4a00661f6f2737248e9420b829c257
hbseog <lllhades@naver.com>
2023-05-19 20:14:35 +0900

    2023-05-19 이미지 업로드 및 타임라인 구성

--- src/main/java/com/example/firstproject/entity/Follow.java
+++ src/main/java/com/example/firstproject/entity/Follow.java
@@ -1,4 +1,18 @@
 package com.example.firstproject.entity;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="following")
+    private Member following;
+
+    @ManyToOne
+    @JoinColumn(name="follower")
+    private Member follower;
 }
--- src/main/java/com/example/firstproject/entity/Member.java
+++ src/main/java/com/example/firstproject/entity/Member.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.List;
+
 @Entity
 @ToString
 @Getter
@@ -26,19 +28,23 @@ public class Member {
     private String email;
     @Column(name = "PASS")
     private String pass;
-//    @Column(name = "CONFIRM_PASS")
+    //    @Column(name = "CONFIRM_PASS")
 //    private String confirm_pass;
     @Column(name = "loginstatus")
     private String loginstatus;
-//    @Column(name = "BRITH")
+    //    @Column(name = "BRITH")
 //    private String birth;
 //    @Column(name = "PHONE")
 //    private String phone;
+    @OneToMany(mappedBy = "follower", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Follow> followers;
+    @OneToMany(mappedBy = "following", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Follow> followings;
 
-    public Member(String name, String email,String pass/*, String confirm_pass, String birth, String phone*/){
-        this.name=name;
-        this.email=email;
-        this.pass=pass;
+    public Member(String name, String email, String pass/*, String confirm_pass, String birth, String phone*/) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
 //        this.confirm_pass=confirm_pass;
 //        this.birth=birth;
 //        this.phone=phone;
